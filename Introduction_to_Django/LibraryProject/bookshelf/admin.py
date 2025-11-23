4f8d96cef29c58cc95dc40516a8d5e37a4d85f77
Farouk Adegboyega <moz.codde@gmail.com>
2025-11-23 22:32:52 +0100

    Updated the admin page to allow search filters and custom display

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -3,4 +3,7 @@ from .models import Book
 
 
 # Register your models here.
-admin.site.register(Book)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "publication_year")
+    list_filter = ("title", "author", "publication_year")
