78066b166c1ec01a16a818c8fad77d17f93657b7
Farouk Adegboyega <moz.codde@gmail.com>
2025-11-23 19:03:15 +0100

    Added the  app to the project settings

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # custom APPs
+    'bookshelf.apps.BookshelfConfig',
 ]
 
 MIDDLEWARE = [
