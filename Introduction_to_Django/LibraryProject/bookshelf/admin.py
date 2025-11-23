28365d1b93c9741630e7bd73572129b3d071f95c
Farouk Adegboyega <moz.codde@gmail.com>
2025-11-23 22:40:20 +0100

    Added the  variable to enable search capabilities

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -6,4 +6,5 @@ from .models import Book
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "publication_year")
-    list_filter = ("title", "author", "publication_year")
+    list_filter = ("author", "publication_year")
+    search_fields = ("title", "author", "publication_year")
