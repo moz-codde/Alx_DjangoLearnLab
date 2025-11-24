23963614937c39e0543a0cd53c22f53a940e8a4a
Farouk Adegboyega <moz.codde@gmail.com>
2025-11-24 11:59:21 +0100

    Added the string representation of the models

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -4,17 +4,29 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
+    def __repr__(self):
+        return self.name
+
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+
+    def __repr__(self):
+        return f"{self.title} by {self.author}"
 
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book, related_name="libraries")
 
+    def __repr__(self):
+        return f"<Library> {self.name}"
+
 
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name="librarian")
+
+    def __repr__(self):
+        return self.name
