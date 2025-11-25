4c0402433ba70f0fdf1460a097718660e8d9c49f
Farouk Adegboyega <moz.codde@gmail.com>
2025-11-25 17:21:16 +0100

    Added more details to the views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,13 +7,15 @@ from .models import Book, Library
 # Create your views here.
 def list_all_books(request):
     books = Book.objects.all()
-    return books
+    return render(request, "templates/list_books.html", context={"books": books})
     
 
 class LibraryDetailView(DetailView):
     model = Library
     context_object_name = "library"
+    template_name = "templates/library_detail.html"
 
 
 class LibraryListView(ListView):
     queryset = Library.books.all()
+    template_name = "templates/library_detail.html"
