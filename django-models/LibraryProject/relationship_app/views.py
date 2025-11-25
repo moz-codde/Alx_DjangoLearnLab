c9c5cac9b6d82c19c38941e2931bf6e18eb130eb
Farouk Adegboyega <moz.codde@gmail.com>
2025-11-25 17:18:32 +0100

    Created templates for ,  view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic import DetailView
 
-from .models import Book
+from .models import Book, Library
 
 # Create your views here.
 def list_all_books(request):
     books = Book.objects.all()
-    result = [f"{book.title} by {book.author}" for book in books]
-    return str(result)
+    return books
+    
+
+class LibraryDetailView(DetailView):
+    model = Library
+    context_object_name = "library"
+
+
+class LibraryListView(ListView):
+    queryset = Library.books.all()
