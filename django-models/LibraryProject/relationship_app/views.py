86b0ff33224ab87db1b58f4948fa1e6599cff14b
Farouk Adegboyega <moz.codde@gmail.com>
2025-11-25 17:25:42 +0100

    fix

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic import DetailView
 
-from .models import Book, Library
+from .models import Library, Book
 
 # Create your views here.
 def list_all_books(request):
