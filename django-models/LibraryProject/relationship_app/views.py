55e0d5066139dab5ed2de61bd0d023ba50c9220a
Farouk Adegboyega <moz.codde@gmail.com>
2025-11-25 15:51:45 +0100

    Added views to list all books in a library

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from .models import Book
+
 # Create your views here.
+def list_all_books(request):
+    books = Book.objects.all()
+    result = [f"{book.title} by {book.author}" for book in books]
+    return str(result)
