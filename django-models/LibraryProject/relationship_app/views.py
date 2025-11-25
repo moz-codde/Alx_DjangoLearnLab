702f49c238d23dfd14e9a87618b331e27d33b11e
Farouk Adegboyega <moz.codde@gmail.com>
2025-11-25 17:24:22 +0100

    Created a subdirectory for each app's templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,16 +6,17 @@ from .models import Book, Library
 
 # Create your views here.
 def list_all_books(request):
+    """This view should render a simple text list of book titles and their authors."""
     books = Book.objects.all()
-    return render(request, "templates/list_books.html", context={"books": books})
+    return render(request, "templates/relationship_app/list_books.html", context={"books": books})
     
 
 class LibraryDetailView(DetailView):
     model = Library
     context_object_name = "library"
-    template_name = "templates/library_detail.html"
+    template_name = "templates/relationship_app/library_detail.html"
 
 
 class LibraryListView(ListView):
     queryset = Library.books.all()
-    template_name = "templates/library_detail.html"
+    template_name = "templates/relationship_app/library_detail.html"
