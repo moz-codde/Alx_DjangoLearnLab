3c7ae55c3e5b4dbad5d7215795506c6b5ce1bb3b
Farouk Adegboyega <moz.codde@gmail.com>
2025-11-26 00:18:47 +0100

    sighs

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import user_passes_test, login_required
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
@@ -47,16 +47,19 @@ def is_member(user):
     return user.is_authenticated and hasattr(user, "userprofile") and user.userprofile.role == "Member"
 
 
+@login_required
 @user_passes_test(is_admin)
 def admin_view(request):
     return render(request, "relationship_app/admin_view.html")
 
 
+@login_required
 @user_passes_test(is_librarian)
 def librarian_view(request):
     return render(request, "relationship_app/librarian_view.html")
 
 
+@login_required
 @user_passes_test(lambda user: user.userprofile.role=="Member")
 def member_view(request):
     return render(request, "relationship_app/member_view.html")
