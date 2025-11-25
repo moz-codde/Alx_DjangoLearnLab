ee68a2e07de1a89772e84b221677e1fd412fa5dc
Farouk Adegboyega <moz.codde@gmail.com>
2025-11-25 17:33:33 +0100

    fix

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from .views import list_all_books, LibraryDetailView, LibraryListView
+
+
+app_name = "relationship_app"
+
+urlpatterns = [
+    path("", LibraryListView.as_view(), name="library_list_view"),
+    path("books/", list_all_books, name="books_list"),
+    path("library/<pk>/", LibraryDetailView.as_view(), name="library_detail_view"),
+]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 
 from .models import Library, Book
 
